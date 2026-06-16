7304689d81caed914246d46c3b26a9dede122543
Sushmit1408 <sushmitpatil1@gmail.com>
2026-06-16 08:46:00 +0530

    react app

---
+++ src/components/ProductCard.js
@@ -0,0 +1,42 @@
+export function ProductCard() {
+  const product = {
+    imageSrc: "images/cell-phone.png",
+    title: "IPhone 17 Pro",
+    specification: [
+      "A21 Pro chip with 16-core CPU",
+      "3x ir 5x telephonic camera",
+      "Up to 48 hours video playback",
+    ],
+    price: 999,
+  };
+
+  function getProductTitle() {
+    return product.title;
+  }
+
+  return (
+    <article
+      style={{
+        border: "1px solid white",
+        borderRadius: "8px",
+        padding: "16px",
+        textAlign: "center",
+      }}
+    >
+      <h2>{getProductTitle()}</h2>
+      <img
+        src={product.imageSrc}
+        alt={product.title}
+        width="128px"
+        height="128px"
+      />
+      <p>Specification:</p>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>{product.specification[0]}</li>
+        <li>{product.specification[1]}</li>
+        <li>{product.specification[2]}</li>
+      </ul>
+      <button>Buy (From ${product.price})</button>
+    </article>
+  );
+}
